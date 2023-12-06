db2b6a04ea0a743d0b6176804a8f04eb2966d6b2
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-12-05 19:01:11 -0500

    first commit

---
+++ routes/api/auth.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { validateBody } = require("../../middlewares");
+const { schemas } = require("../../models/user");
+const router = express.Router();
+
+router.post("/users/register", validateBody(schemas.registerSchema));
+module.exports = router;
