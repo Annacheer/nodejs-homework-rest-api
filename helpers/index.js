f0f84061539ca3761598e336df94eb8fdca3a958
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-12-21 15:29:30 -0500

    verifyEmail, resendVerifyEmail

--- helpers/index.js
+++ helpers/index.js
@@ -3,8 +3,11 @@ const ctrlWrapper = require("./ctrlWrapper");
 const HttpError = require("./HttpError");
 const handleMongooseError = require("./handleMongooseError");
 
+const transport = require("./sendEmail");
+
 module.exports = {
   ctrlWrapper,
   HttpError,
   handleMongooseError,
+  transport,
 };
